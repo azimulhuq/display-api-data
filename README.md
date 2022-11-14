de3740a0b569e2292d1d9f4ffc0b98f09aaccb4b
Azimul Huq <azimulhuqbd@gmail.com>
2022-11-14 06:02:51 -0600

    Modified css files and refactored some javascript codes.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# display-api-data
+# Used Star Wars API endpoint- https://swapi.dev/
+
+# Test the code into google chrome browser.
+
+# index.html is the main page and also have separate page for each modules.
+
+# Each module section such as people module have films and vehicle urls. If you click one of these urls it shows single view page.
+
+# You can navigate to all 3 modules through any page.
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -14,162 +14,245 @@ window.addEventListener("scroll", () => {
   });
 });
 
-/* ################# Render Peoples on index.html ################# */
-async function renderPeoplesOnHome() {
+/* ################# Render Peoples, Films & Vehicles Module on index.html ################# */
+
+const homeModules = [
+  "https://swapi.dev/api/people/",
+  "https://swapi.dev/api/films/",
+  "https://swapi.dev/api/vehicles/",
+];
+
+async function renderModulesOnHome(url, limit) {
+  let message = "";
+
+  if (url.split("/").at(-2) === "people") {
+    message =
+      "There was a problem with fetching the peoples module content in home page.";
+  } else if (url.split("/").at(-2) === "films") {
+    message =
+      "There was a problem with fetching the films module content in home page.";
+  } else {
+    message =
+      "There was a problem with fetching the vehicles module content in home page.";
+  }
+
   try {
-    const response = await fetch("https://swapi.dev/api/people/");
+    const response = await fetch(url);
     const data = await response.json();
-    getPeoplesOnHome(data);
+    getModulesOnHome(data, limit);
   } catch (error) {
-    console.log(
-      "There was a problem with fetching the peoples content in home page."
-    );
+    console.log(message);
   }
 }
 
-function getPeoplesOnHome(peoples) {
+function getModulesOnHome(modules, limit) {
   let html = "";
+  let homeModuleHeading = "";
+  let sectionContainer = "";
+  let htmlPage = "";
+  let componentArrayName = "";
+  let segment = "";
+  let sectionsegment = "";
 
-  peoples.results.forEach((people) => {
-    if (people.films.length > 3) {
-      let sectionsegment = `
-        <article id="" class="people h-people">
-          <h3>${people.name}</h3>
-          <p><strong>Height: </strong>${people.height}</p>
-          <p><strong>Hair Color: </strong>${people.hair_color}</p>
-          <p><strong>Birth Year: </strong>${people.birth_year}</p>
-          <p><strong>Gender: </strong>${people.gender}</p>
-          <p><strong>Films: </strong><span class="link-film"><a href="../../templates/films.html" target="_blank">${people.films.join(
-            ", "
-          )}</a></span></p>
-          <p><strong>Vehicles: </strong><span class="link-vehicle">${
-            people.vehicles.length > 0 ? getElements(people.vehicles) : "N/A"
-          }</span></p>
-        </article>
+  modules.results.forEach((moduleHome) => {
+    if (moduleHome.url.split("/").at(-3) === "people") {
+      componentArrayName = moduleHome.films;
+      segment = `
+          <article class="people h-people">
+            <h3>${moduleHome.name}</h3>
+            <p><strong>Height: </strong>${moduleHome.height}</p>
+            <p><strong>Hair Color: </strong>${moduleHome.hair_color}</p>
+            <p><strong>Birth Year: </strong>${moduleHome.birth_year}</p>
+            <p><strong>Gender: </strong>${moduleHome.gender}</p>
+            <p><strong>Films: </strong><span class="link-film">${getElements(
+              moduleHome.films
+            )}</span></p>
+            <p><strong>Vehicles: </strong><span class="link-vehicle">${
+              moduleHome.vehicles.length > 0
+                ? getElements(moduleHome.vehicles)
+                : "N/A"
+            }</span></p>
+          </article>
         `;
+      sectionContainer = document.querySelector("#render-people");
+      homeModuleHeading = "Peoples";
+      htmlPage = "peoples";
+    } else if (moduleHome.url.split("/").at(-3) === "films") {
+      componentArrayName = moduleHome.vehicles;
+      segment = `
+          <article class="movie h-movie">
+              <h3>${moduleHome.title}</h3>
+              <p><strong>Director: </strong>${moduleHome.director}</p>
+              <p><strong>Producer: </strong>${moduleHome.producer}</p>
+              <p><strong>Release Date: </strong>${moduleHome.release_date}</p>
+              <p><strong>Plot: </strong>${moduleHome.opening_crawl
+                .slice(0, 150)
+                .concat("...")}</p>
+              <p class="fav-char"><strong>Characters: </strong><span class="film">${getElements(
+                moduleHome.characters.slice(0, 8).map((char) => {
+                  return char;
+                })
+              )}</a></span></p>
+          </article>        
+        `;
+      sectionContainer = document.querySelector("#render-film");
+      homeModuleHeading = "Films";
+      htmlPage = "films";
+    } else {
+      componentArrayName = moduleHome.films;
+      segment = `
+          <article class="vehicle h-vehicle">
+              <h3>${moduleHome.name}</h3>
+              <p><strong>Model: </strong>${moduleHome.model}</p>
+              <p><strong>Manufacturer: </strong>${
+                moduleHome.manufacturer
+              }</p>            
+              <p><strong>Passengers: </strong>${
+                moduleHome.passengers
+              }</p>            
+              <p><strong>Cost: </strong>${
+                moduleHome.cost_in_credits
+              }</p>            
+              <p><strong>Films: </strong>${getElements(
+                moduleHome.films
+              )}</p>            
+          </article>       
+        `;
+      sectionContainer = document.querySelector("#render-vehicle");
+      homeModuleHeading = "Vehicles";
+      htmlPage = "vehicles";
+    }
+
+    if (componentArrayName.length > limit) {
+      sectionsegment = segment;
 
       html += sectionsegment;
     }
   });
 
-  let container = document.querySelector("#render-people");
-  container.innerHTML = `
-    <h2>Peoples</h2>
+  sectionContainer.innerHTML = `
+    <h2>${homeModuleHeading}</h2>
     ${html}
-    <article class="view-all"><p><a href="templates/peoples.html">View All Peoples</a></p></article>
+    <article class="view-all"><p><a href="templates/${htmlPage}.html">View All ${homeModuleHeading}</a></p></article>
    `;
 }
 
 function getElements(arrayData) {
+  let holder = "";
+  let href = "";
+
+  if (moduleName(arrayData) === "films") {
+    href = "../../views/film-view.html";
+  } else if (moduleName(arrayData) === "vehicles") {
+    href = "../../views/vehicle-view.html";
+  } else {
+    href = "../../views/people-view.html";
+  }
+
   for (let index = 0; index < arrayData.length; index++) {
-    return `<a href="../../views/vehicles-views.html" data-url="${arrayData[index]}">${arrayData[index]}</a>`;
+    const id = arrayData[index].split("/").at(-2);
+
+    let elemHolder = `<a href="${href}" onclick="${showData(
+      arrayData[index]
+    )}" data-id="${id}">${arrayData[index]}</a>`;
+
+    holder += elemHolder + ", ";
   }
+
+  return holder;
 }
 
-renderPeoplesOnHome();
+function moduleName(moduleNames) {
+  let first = "";
+  if (moduleNames.length > 0) {
+    first = moduleNames[0];
+  }
 
-/* ################# Render Films on index.html ################# */
+  return first.split("/").at(-3);
+}
 
-async function renderMoviesOnHome() {
+async function showData(link) {
   try {
-    const response = await fetch("https://swapi.dev/api/films/");
+    const response = await fetch(link);
     const data = await response.json();
-    getMoviesOnHome(data);
+    getDetailsOnModuleLinkClick(data);
   } catch (error) {
     console.log(
-      "There was a problem with fetching the movies content on home page."
+      "There was a problem with fetching the vehicles content in peoples link click."
     );
   }
 }
 
-async function getMoviesOnHome(movies) {
-  let html = "";
+function getDetailsOnModuleLinkClick(module) {
+  let sectionsegment = "";
+  let headingView = "";
+  let container = "";
 
-  movies.results.forEach((movie) => {
-    if (movie.vehicles.length > 5) {
-      let sectionsegment = `        
-            <article class="movie h-movie">
-                <h3>${movie.title}</h3>
-                <p><strong>Director: </strong>${movie.director}</p>
-                <p><strong>Producer: </strong>${movie.producer}</p>
-                <p><strong>Release Date: </strong>${movie.release_date}</p>
-                <p><strong>Plot: </strong>${movie.opening_crawl
-                  .slice(0, 150)
-                  .concat("...")}</p>
-                <p class="fav-char"><strong>Characters: </strong><span class="film"><a href="../../templates/peoples.html" target="_blank">${movie.characters
-                  .slice(0, 8)
-                  .map((char) => {
-                    return char;
-                  })
-                  .join(", ")}</a></span></p>
-            </article>        
-            `;
-
-      html += sectionsegment;
-    }
-  });
-
-  let moviecontainer = document.querySelector("#render-film");
-  moviecontainer.innerHTML = `    
-      <h2>Films</h2>
-      ${html}
-      <article class="view-all"><p><a href="templates/films.html">View All Films</a></p></article>      
-     `;
-}
-
-renderMoviesOnHome();
-
-/* ################# Render Vehicles on index.html ################# */
-
-async function renderVehiclesOnHome() {
-  try {
-    const response = await fetch("https://swapi.dev/api/vehicles/");
-    const data = await response.json();
-    getVehiclesOnHome(data);
-  } catch (error) {
-    console.log(
-      "There was a problem with fetching the vehicles content on home page."
-    );
-  }
-}
-
-async function getVehiclesOnHome(vehicles) {
-  let html = "";
-
-  vehicles.results.forEach((vehicle) => {
-    if (vehicle.films.length > 1) {
-      let sectionsegment = `
-          <article class="vehicle h-vehicle">
-              <h3>${vehicle.name}</h3>
-              <p><strong>Model: </strong>${vehicle.model}</p>
-              <p><strong>Manufacturer: </strong>${
-                vehicle.manufacturer
-              }</p>            
-              <p><strong>Passengers: </strong>${
-                vehicle.passengers
-              }</p>            
-              <p><strong>Cost: </strong>${
-                vehicle.cost_in_credits
-              }</p>            
-              <p><strong>Films: </strong><a href="../../templates/films.html" target="_blank">${vehicle.films.join(
+  if ("films" === module.url.split("/").at(-3)) {
+    headingView = "Film View";
+    sectionsegment = `
+          <article class="movie v-film">
+              <h3>${module.title}</h3>
+              <p><strong>Director: </strong>${module.director}</p>
+              <p><strong>Producer: </strong>${module.producer}</p>
+              <p><strong>Release Date: </strong>${module.release_date}</p>
+              <p><strong>Plot: </strong>${module.opening_crawl}</p>
+              <p class="fav-char"><strong>Characters: </strong><span class="film"><a href="../../templates/peoples.html" target="_blank">${module.characters.join(
                 ", "
-              )}</a></p>            
+              )}</a></span></p>
+          </article>  
+        `;
+    container = document.querySelector("#film-view");
+  } else if ("vehicles" === module.url.split("/").at(-3)) {
+    headingView = "Vehicle View";
+    sectionsegment = `
+          <article class="vehicle v-vehicle">
+            <h3>${module.name}</h3>
+            <p><strong>Model: </strong>${module.model}</p>
+            <p><strong>Manufacturer: </strong>${
+              module.manufacturer
+            }</p>            
+            <p><strong>Passengers: </strong>${module.passengers}</p>            
+            <p><strong>Cost: </strong>${module.cost_in_credits}</p>            
+            <p><strong>Films: </strong><a href="../templates/films.html" target="_blank">${module.films.join(
+              ", "
+            )}</a></p>                    
           </article>
-          `;
+        `;
+    container = document.querySelector("#vehicle-view");
+  } else {
+    headingView = "Character View";
+    sectionsegment = `
+          <article class="people h-people">
+            <h3>${module.name}</h3>
+            <p><strong>Height: </strong>${module.height}</p>
+            <p><strong>Hair Color: </strong>${module.hair_color}</p>
+            <p><strong>Birth Year: </strong>${module.birth_year}</p>
+            <p><strong>Gender: </strong>${module.gender}</p>
+            <p><strong>Films: </strong><span class="link-film"><a href="../templates/films.html" target="_blank">${module.films.join(
+              ", "
+            )}</a></span></p>
+            <p><strong>Vehicles: </strong><span class="link-vehicle">${
+              module.vehicles.length > 0
+                ? `<a href="../templates/vehicles.html" target="_blank">
+                  ${module.vehicles.join(", ")}
+                </a>`
+                : "N/A"
+            }</span></p>           
+          </article>
+        `;
+    container = document.querySelector("#people-view");
+  }
 
-      html += sectionsegment;
-    }
-  });
-
-  let container = document.querySelector("#render-vehicle");
   container.innerHTML = `
-    <h2>Vehicles</h2>
-    ${html}
-    <article class="view-all"><p><a href="templates/vehicles.html">View All Vehicles</a></p></article>
+    <h2>${headingView}</h2>
+    ${sectionsegment}    
    `;
 }
 
-renderVehiclesOnHome();
+renderModulesOnHome(homeModules[0], 3);
+renderModulesOnHome(homeModules[1], 5);
+renderModulesOnHome(homeModules[2], 1);
 
 /* ################# Peoples Page ################# */
 
